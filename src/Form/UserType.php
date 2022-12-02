3684e70435c248b1366b44dc953ffe9f7f64b08f
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2022-12-02 20:57:58 +0100

    Registration Login some css

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
@@ -42,6 +43,7 @@ class UserType extends AbstractType
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Validez votre mot de passe'],
             ])
+            ->add('add', SubmitType::class)
         ;
     }
 
