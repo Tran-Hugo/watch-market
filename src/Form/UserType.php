8b1c62afdce21bd584769f50ad0bf4da8d28d7a9
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2022-12-02 01:35:49 +0100

    added user controller and form

---
+++ src/Form/UserType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname',TextType::class, [
+                'required' => true,
+                'constraints'=> [
+                    new NotBlank([
+                        'message'=>'Le champ est requis'
+                        ])
+                ]
+            ])
+            ->add('lastname',TextType::class, [
+                'required' => true,
+                'constraints'=> [
+                    new NotBlank([
+                        'message'=>'Le champ est requis'
+                        ])
+                ]
+            ])
+            ->add('email',EmailType::class)
+            ->add('password',RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Validez votre mot de passe'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
