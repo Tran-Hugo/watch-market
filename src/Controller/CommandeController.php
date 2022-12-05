ced65458438c3892a163b6a8b29a0d621a77d567
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2022-12-05 22:06:36 +0100

    cart added

---
+++ src/Controller/CommandeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CommandeController extends AbstractController
+{
+    #[Route('/order/new', name: 'order', methods:['POST'])]
+    public function index(Request $request, ArticleRepository $articleRepo): Response
+    {
+        $cart = json_decode($request->getContent(),true);
+        $idList = ['id'=>[]];
+        foreach($cart["cart"] as $item){
+            array_push($idList["id"],$item["id"]);
+        };
+        $articles = $articleRepo->findBy($idList);
+        dd($articles);
+        return $this->json();
+    }
+}
