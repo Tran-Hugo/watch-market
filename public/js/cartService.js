c333fe76eaacb6b2cb0a915199289142d9a6292c
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2022-12-05 01:14:31 +0100

    modal + some cart changes

--- public/js/cartService.js
+++ public/js/cartService.js
@@ -1,39 +1,52 @@
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
-function addCart(idArticle, idUser){
-    let cart = JSON.parse(localStorage.getItem('cart'));
-    if(cart){
-       let cartJson = cart;
-       if(idUser){
-        cartJson['idUser']=idUser;
-       };
-       let articleExist = cartJson['cart'].find(obj=>obj.id === idArticle);
-       if(articleExist){
-        articleExist['quantity']++
-        cartJson=JSON.stringify(cartJson);
-        localStorage.setItem('cart',cartJson);
-       } else {
-        let article = {
-            id:idArticle,
-            quantity:1
-        };
-        cartJson['cart'].push(article);
-        cartJson=JSON.stringify(cartJson);
-        localStorage.setItem('cart',cartJson);
-       }
-    } else {
-        let cartJson={
-            idUser:null,
-            cart:[]
-        };
-        if(idUser){
-            cartJson['idUser']=idUser;
-        };
-        let article = {
-            id:idArticle,
-            quantity:1
-        }
-        cartJson["cart"].push(article);
-        cartJson=JSON.stringify(cartJson);
-        localStorage.setItem('cart',cartJson);
+function addCart(idArticle, idUser) {
+  let cart = JSON.parse(localStorage.getItem("cart"));
+  if (cart) {
+    let cartJson = cart;
+    if (idUser) {
+      cartJson["idUser"] = idUser;
     }
+    let articleExist = cartJson["cart"].find((obj) => obj.id === idArticle);
+    if (articleExist) {
+      articleExist["quantity"]++;
+      cartJson = JSON.stringify(cartJson);
+      localStorage.setItem("cart", cartJson);
+      modal.style.display = "block";
+    } else {
+      let article = {
+        id: idArticle,
+        quantity: 1,
+      };
+      cartJson["cart"].push(article);
+      cartJson = JSON.stringify(cartJson);
+      localStorage.setItem("cart", cartJson);
+      modal.style.display = "block";
+    }
+  } else {
+    let cartJson = {
+      idUser: null,
+      cart: [],
+    };
+    if (idUser) {
+      cartJson["idUser"] = idUser;
+    }
+    let article = {
+      id: idArticle,
+      quantity: 1,
+    };
+    cartJson["cart"].push(article);
+    cartJson = JSON.stringify(cartJson);
+    localStorage.setItem("cart", cartJson);
+    modal.style.display = "block";
+  }
 }
